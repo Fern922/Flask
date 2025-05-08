1f3fe4d21c570cdf1310458308934edf7e93e825
Fern922 <fernandodelariva911@gmail.com>
2025-05-08 09:58:32 +0800

    Update App.py

--- App.py
+++ App.py
@@ -4,10 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Fernando Dela Riva</h1>'
-   '<h1>3rd Year BSIT</h1>'
-    '<h1>System Integration and Architecture</h1>'
-'<h1>Semi Final Exam</h1>'
+    return '<h1>Fernando Dela Riva - 3rd Year BSIT - System Integration and Architecture - Semi Final Exam</h1>'
 
 port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0', port=port)
